eff5d359a6e137ea2cf74dda636f28d27abb19da
walidAiachi <walidaiachi@gmail.com>
2023-01-20 21:23:09 +0100

    create ejb gestionnaire , edit persistence

---
+++ src/main/java/ejb/GestionnaireCompte.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejb;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Stateless;
+
+/**
+ *
+ * @author walid
+ */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",              // nom et
+    password="123456", // mot de passe que vous avez donnés lors de la création de la base de données
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true"
+    }
+)
+
+@Stateless
+public class GestionnaireCompte {
+    
+}
